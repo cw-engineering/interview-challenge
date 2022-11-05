d59b588fee6d36f5effbaa113d6b1f26fd5587fc
Ondrej Urik <o.urik@craneware.com>
2022-11-05 10:44:34 +0000

    Added the challenge

---
+++ csharp/src/StringUtilities/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CodingChallenge.StringUtilities;
+
+public class StringExtensions
+{
+    public static string Reverse(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static string Sanitize(string input, IEnumerable<char> invalidChars)
+    {
+        throw new NotImplementedException();
+    }
+}
